74b4ec87a079a23e409059267796f0d8325ba97f
Rahul Sadare <132609391+sadarerahul@users.noreply.github.com>
2025-04-23 18:47:17 +0530

    Update portfolios.py

--- portfolios.py
+++ portfolios.py
@@ -1,6 +1,6 @@
-
 import chromadb
 
 def init_resume_collection():
-    chroma_client = chromadb.PersistentClient(path="./chroma_db")
-    return chroma_client.get_or_create_collection(name="resume_collection")
+    chroma_client = chromadb.Client()
+    collection = chroma_client.get_or_create_collection(name="resume_collection")
+    return collection
