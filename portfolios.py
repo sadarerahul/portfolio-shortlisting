9d73e617213f0b26727605ae2ead018ce4e7055b
Rahul Sadare <132609391+sadarerahul@users.noreply.github.com>
2025-04-23 17:56:38 +0530

    Update portfolios.py

--- portfolios.py
+++ portfolios.py
@@ -1,15 +1,6 @@
 import chromadb
 
-def init_portfolios():
+def init_resume_collection():
     chroma_client = chromadb.Client()
-    collection = chroma_client.create_collection(name="portfolios")
-    collection.add(
-        documents=[
-            "Rahul is a Data Scientist with 2 years experience in ML, GenAI, and DL.",
-            "Ramraje is a Python developer with 3 years experience, expert in ML.",
-            "Soham specializes in cloud computing, ML, DL.",
-            "Om is a web developer skilled in Python, HTML, Java, JavaScript."
-        ],
-        ids=["p1", "p2", "p3", "p4"]
-    )
+    collection = chroma_client.get_or_create_collection(name="resume_collection")
     return collection
